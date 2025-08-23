db5761fd98d5580940454a62aba32390c4a4c512
vitlobo <awhite1196@proton.me>
2025-08-23 12:40:56 -0400

    CH4-L2: Working on parsing blocks of string into inline strings that can then be converted.

---
+++ src/markdown_blocks.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def block_to_block_type(block_type):
+    pass
+
+
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
