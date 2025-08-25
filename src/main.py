94a5f2f9c776f0a9544bc3dfe5b77263bf02a5f6
vitlobo <awhite1196@proton.me>
2025-08-25 14:26:04 -0400

    CH5-L4: Added additional .md content to be converted to HTML and served. Added ability to recursively search and convert all files in the content location.

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ import os
 import shutil
 
 from copystatic import copy_files_recursive
-from generate_page import generate_page
+from generate_page import generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 def main():
@@ -16,7 +18,8 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    generate_page("./content/index.md", "./template.html", "./public/index.html")
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 
 if __name__ == "__main__":
