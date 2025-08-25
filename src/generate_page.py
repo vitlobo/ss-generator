5ae87c392bbbfaf8ae752c1f1069a7011354ce7d
vitlobo <awhite1196@proton.me>
2025-08-25 11:31:47 -0400

    CH5-L2: Added generate page function which takes a .md file as input converts the content to html and writes the html into a template html file.

---
+++ src/generate_page.py
@@ -0,0 +1,30 @@
+import os
+
+from markdown_blocks import markdown_to_html_node
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path, "r") as f:
+        content = f.read()
+    with open(template_path, "r") as f:
+        template = f.read()
+
+    template_html = markdown_to_html_node(content).to_html()
+
+    title = extract_title(content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", template_html)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    with open(dest_path, "w") as f:
+        f.write(template)
+
+
+def extract_title(markdown):
+    if not markdown.startswith("# "):
+        raise ValueError("no title found")
+    header = markdown.split("\n")[0]
+    return header.lstrip("#").strip()
