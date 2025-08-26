45349f79098dcd6c40502fc359049f1c8cef3802
vitlobo <awhite1196@proton.me>
2025-08-25 20:57:10 -0400

    Readability edits

--- src/main.py
+++ src/main.py
@@ -6,26 +6,26 @@ from copystatic import copy_files_recursive
 from generate_page import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_docs = "./docs"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
 
 def main():
+    basepath = default_basepath
     if len(sys.argv) > 1:
         basepath = sys.argv[1]
-    else:
-        basepath = "/"
 
     print("Deleting public directory...")
-    if os.path.exists(dir_path_docs):
-        shutil.rmtree(dir_path_docs)
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
     print("Copying static files to public directory...")
-    copy_files_recursive(dir_path_static, dir_path_docs)
+    copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, basepath, template_path, dir_path_docs)
+    generate_pages_recursive(dir_path_content, basepath, template_path, dir_path_public)
 
 
 if __name__ == "__main__":
